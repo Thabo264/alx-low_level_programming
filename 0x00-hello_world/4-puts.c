e5757c8b333244a3e5deb8017d3d4ba882598eef
Thabo264 <thabosenong264@gmail.com>
2023-07-13 16:04:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints a line with puts functions
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingua puzzle);
+Return (0);
+}
