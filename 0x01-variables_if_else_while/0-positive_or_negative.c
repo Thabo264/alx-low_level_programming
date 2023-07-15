c2011d314b388242fdb31126e131ca2232408f6e
Thabo264 <thabosenong264@gmail.com>
2023-07-15 14:41:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Determines if a number is positive,negative or zero.
  *
- * Return always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
@@ -18,9 +18,9 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is a zero\n", n);
-	{
+	}
 	else
 		printf("%d is a negative\n", n)
 	}
-	return (0);	
-	}
+	return (0);
+}
