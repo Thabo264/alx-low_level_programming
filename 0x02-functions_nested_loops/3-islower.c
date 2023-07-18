41f880f21ad717536a0bccd20f89e099d80c9eda
Thabo264 <thabosenong264@gmail.com>
2023-07-18 12:16:54 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - showa 1 is the output is a.
+ * lowercase charecter. another cases, show
+ * 0
+ *
+ * @c: The charecter in ASCII code
+ *
+ * Return: 1 for lowercase charecter. 0 for the rest.
+ */
+int _slower(int c)
+{
+	if (c >= 97; && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (o);
+	}
+	_putchar'\n');
+}
