4dd71251cc438dd24ec35446ed7f2017480887ba
Thabo264 <thabosenong264@gmail.com>
2023-07-19 11:58:47 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - shows 1 if the output is 0.
+ * letter another cases 0
+ *
+ * @c: the charecter in ASCII code
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 99))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
